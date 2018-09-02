c5b84689e71bcd3246611a46bebb06cb8e894d9a
guohaisheng <guohai19920227@163.com>
2018-09-02 20:09:39 +0800

    配资路由

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/css/index.css'
 
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes: []
+});
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App),
+    router
 })
